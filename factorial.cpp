4160e0fc40da540dd18e721ce681d96892c7a50e
Omprakash02092002 <om02092002@gmail.com>
2024-09-03 23:46:30 +0530

    add file

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,23 @@
-#include<iostream>
+/*   Print the first N factorial numbers.
+1! = 1
+2! = 2 * 1!
+3! = 3 * 2!            */
+
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number :" ;
-    cin>>n;
+    cout << "Enter the numbers : ";
+    cin >> n;
 
-    for(int i;i<=n;i++)
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
     {
-        f
+        factorial *= i;
+        cout << factorial << endl;
     }
+    cout << endl;
 
-
+    return 0;
 }
