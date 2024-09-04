37d72146f25ae7efed4215874a3dc38788b3b539
Omprakash02092002 <om02092002@gmail.com>
2024-09-05 00:43:00 +0530

    add programe

--- day 2/MCQ.cpp
+++ day 2/MCQ.cpp
@@ -1,5 +1,5 @@
-//---sum of two numbers. 
-
+//---sum of two numbers.
+/*
 #include <iostream>
 using namespace std;
 int main()
@@ -14,3 +14,56 @@ int main()
     cout << "sum : " << sum  << endl;
     return 0;
 }
+*/
+//_____________________________________________________________________________________
+
+// uneary operator
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = a++ ;
+    int c = b-- ;
+
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+    cout << "c : " << c << endl;
+
+    return 0;
+}
+*/
+//___________________________________________________________________________________
+
+//  ----------Arthametic Operators--------------------
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter a : ";
+    cin >> a;
+    cout << "Enter b : ";
+    cin >> b;
+
+    int Addition, Subtruction, Multiplication,  Modulus;
+    double Divide;
+
+    Addition = a + b;
+    Subtruction = a - b;
+    Multiplication = a * b;
+    Divide = a / (double)b;     
+    Modulus = a % b;
+
+    cout << "Addition : " << Addition << endl;
+    cout << "Subtruction: " << Subtruction << endl;
+    cout << "Multiplication: " << Multiplication << endl;
+    cout << "Divide: " << Divide << endl;
+    cout << "MOdulus: " << Modulus << endl;
+
+    return 0;
+}
+
+//__________________________________________________________________________________
