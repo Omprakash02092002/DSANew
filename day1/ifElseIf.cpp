a5b18f25920d5b44286300b65d6a07f74af0d79d
Omprakash02092002 <om02092002@gmail.com>
2024-09-05 23:46:16 +0530

    new file added

--- day1/ifElseIf.cpp
+++ day1/ifElseIf.cpp
@@ -1,5 +1,5 @@
 // Given a person's age ,find if they should get a driving license or not.
-
+/*
 #include <iostream>
 using namespace std;
 int main ()
@@ -23,3 +23,61 @@ int main ()
     }
     return 0;
 }
+*/
+//____________________________________________________________________________________________
+
+// ----- Find Result -----//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout << "enter your marks : " ;
+    cin >> marks;
+
+    if (marks > 100 || marks < 0)
+    {
+        cout << "not valid  :";
+    }
+    else if (marks >= 95)
+    {
+        cout << "A++";
+    }
+    else if (marks >= 90)
+    {
+        cout << "A+";
+    }
+    else if (marks >= 80)
+    {
+        cout << "A";
+    }
+    else if (marks >= 75)
+    {
+        cout << "B+";
+    }
+    else if (marks >= 60)
+    {
+        cout << "B";
+    }
+    else if (marks >= 45)
+    {
+        cout << "C";
+    }
+    else if (marks >= 35)
+    {
+        cout << "D";
+    }
+    else if (marks >= 25)
+    {
+        cout << "E";
+    }
+    else
+    {
+        cout << "fali";
+    }
+
+    return 0;
+}
+
+//_______________________________________________________________________________________
